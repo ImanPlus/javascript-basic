432084cddd575f7719a6c85c029c9832c5f4567b
Iman Jalali <i.jalali88@gmail.com>
2024-02-12 16:57:39 +0330

    write first and second solution for trim command

--- trim.js
+++ trim.js
@@ -1,6 +1,31 @@
+// The first solution
 const str = "   salam khoobi    chetori che khabar   ";
-let trimStr = str.replace(/^\s+|\s+$/g, "");
+let firstValid = 0;
+let secondeValid = 0;
+
+for (let i = 0; i < str.length; i++) {
+  if (firstValid === 0 && str[i] != " ") {
+    firstValid = i;
+    break;
+  }
+}
+
+for (let i = str.length - 1; i >= 0; i--) {
+  if (secondeValid === 0 && str[i] != " ") {
+    secondeValid = i;
+    break;
+  }
+}
+
+const result = str.slice(firstValid, secondeValid + 1);
+console.log(result);
+
+//Out put ===> salam khoobi    chetori che khabar
+
+// The second solution
+const myStr = "   salam khoobi    chetori che khabar   ";
+let trimStr = myStr.replace(/^\s+|\s+$/g, "");
 
 console.log(trimStr);
 
-//Output ===>salam khoobi    chetori che khabar
+// //Output ===>salam khoobi    chetori che khabar
